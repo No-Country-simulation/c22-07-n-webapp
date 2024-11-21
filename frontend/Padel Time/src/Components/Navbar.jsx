b69d9a985f0464fcdc3ab1370f3a138a9b4cb896
alvarogomez7 <alvaroigomezj96@gmail.com>
2024-11-21 20:31:34 -0300

    Creacion de Navbar y Footer

---
+++ frontend/Padel Time/src/Components/Navbar.jsx
@@ -0,0 +1,19 @@
+
+
+const Navbar = () => {
+  return (
+    <div>
+      <nav>
+        <img src="" alt="logo"/>
+        <ul>
+          <li>Inicio</li>
+          <li>Contacto</li>
+          <li>Reservar cancha</li>
+          <li>Sobre nosotros</li>
+        </ul>
+      </nav>
+    </div>
+  )
+}
+
+export default Navbar
